b373e59bed022d208fd1c02701ee3ac89c7910ff
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-02-25 14:52:22 -0500

    more work on basics

--- idslabel.py
+++ idslabel.py
@@ -14,10 +14,19 @@ class MainWindow:
 
         self.conversation_blocks = None
 
-        self.current_clip = None    # [path, parent_path,
+        self.current_clip = [None]*5    #  [audio_clip_path,     0
+                                        #   parent_audio_path,   1
+                                        #   block_index,         2
+                                        #   clip_index,          3
+                                        #   classification,      4
+                                        #   timestamp]           5
         self.current_block = None
 
+        self.processed_clips_in_curr_block = []
+        self.processed_clips = []
 
+        # list of integers corresponding
+        # to self.conversation_blocks
         self.previously_selected_blocks = []
 
         self.root = master                # main GUI context
@@ -70,8 +79,23 @@ class MainWindow:
         self.ids_var = IntVar()
         self.ids_button = Checkbutton(self.main_frame, text="IDS", variable=self.ids_var)
 
+        self.ads_var = IntVar()
+        self.ads_button = Checkbutton(self.main_frame, text="ADS", variable=self.ads_var)
 
+        self.neither_var = IntVar()
+        self.neither_button = Checkbutton(self.main_frame, text="Neither", variable=self.neither_var)
 
+        self.junk_var = IntVar()
+        self.junk_button = Checkbutton(self.main_frame, text="Junk", variable=self.junk_var)
+
+        self.ids_button.grid(row=2, column=0)
+        self.ads_button.grid(row=3, column=0)
+        self.neither_button.grid(row=4, column=0)
+        self.junk_button.grid(row=5, column=0)
+
+        self.classification_conflict_label = None
+
+        self.clips_processed_label = None
 
     def load_clan(self):
         self.clan_file = tkFileDialog.askopenfilename()
@@ -153,6 +177,8 @@ class MainWindow:
             filtered_conversations.append(conv_block)
             conv_block = []
 
+
+
         return filtered_conversations
 
 
@@ -172,10 +198,48 @@ class MainWindow:
 
 
     def next_clip(self):
-        print "hello"
+
+        if self.ids_var.get() == 1 and self.ads_var.get() == 1:
+            self.classification_conflict_label = Label(self.main_frame,
+                                                       text="can't be both IDS and ADS")
+
+            self.classification_conflict_label.grid(row=6, column=0)
+            return
+
+        if self.classification_conflict_label:
+            self.classification_conflict_label.grid_remove()
+
+        if self.ids_var.get() == 1:
+            self.current_clip[3] = "ids"
+        if self.ads_var.get() == 1:
+            self.current_clip[3] = "ads"
+        if self.neither_var.get() == 1:
+            self.current_clip[3] = "neither"
+        if self.junk_var.get() == 1:
+            self.current_clip[3] = "junk"
+
+        self.reset_classification_buttons()
+
+        self.processed_clips_in_curr_block.append(self.current_clip)
+
+        self.current_clip = self.current_block[self.current_clip[2]]
+
+        self.clips_processed_label = Label(self.main_frame,
+                                           text="{} total clips processed"
+                                           .format(len(self.processed_clips_in_curr_block+\
+                                                       self.processed_clips)))
+        self.clips_processed_label.grid(row=6, column=3)
+
 
     def replay_clip(self):
         print "hello"
+
+    def reset_classification_buttons(self):
+        self.ids_button.deselect()
+        self.ads_button.deselect()
+        self.neither_button.deselect()
+        self.junk_button.deselect()
+
 
 
 
